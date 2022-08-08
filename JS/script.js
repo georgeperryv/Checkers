e75e91c897418c225ec9a0fd31b53fbd00ce9864
George Perry <george.e.perry5@gmail.com>
2022-08-08 10:01:09 -0700

    grid display WIP

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+// var row1 = document.querySelector('.row-1');
+
+// let count = 1;
+// for (i = 0; i < 8; i++){
+//     var button = document.createElement("button");
+//     button.innerHTML = `Do Something ${i}`;
+//     button.setAttribute("class", `button ${i}`);
+
+//     if (count % 2 === 1){
+//         button.style.background = 'white';
+//         count++;
+//         console.log(count);
+//     }
+//     else{
+//         button.style.background = 'black';
+//         count++;
+//     }
+
+//     row1.appendChild(button);
+// }
