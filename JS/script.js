49c7a16f893fe112a6863767bc39166d039269d5
George Perry <george.e.perry5@gmail.com>
2022-08-08 13:27:21 -0700

    Event listener on whole board which returns the element clicked on

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ let colorCount = 1; //counter for the color of the div
 
 for (i = 1; i <= 64; i++){ //add 64 divs of alternating color
     let div = document.createElement("div");
-    div.setAttribute("class", `div ${i}`);
+    div.setAttribute("id", `${i}`);
 
     if (colorCount % 9 === 0){
         console.log("mod 8 " + colorCount);
@@ -27,13 +27,53 @@ for (i = 1; i <= 64; i++){ //add 64 divs of alternating color
         colorCount++;
  
     }
-
    
 
     boarder.appendChild(div);
 }
 
+
+// const element = document.getElementById('47');
+// const position = element.getBoundingClientRect();
+// const x1 = position.left;
+// const x2 = position.right;
+// const y1 = position.top;
+// const y2 = position.bottom;
+
+// console.log(x1, x2, y1, y2);
+
+
+
 ///////////////////////////////////////////////////////////////////
 
-     
+class piece {
+    constructor(color, kingStatus){
+        this.color = color;
+        this.kingStatus = false;
+    }
+
+    //getter
+
+//     getPosition {
+
+//     }
+
+// }
+
+
+// function returnClickPosition{
+
+
+
+}
+
+boarder.addEventListener('click', function(element) {
+    // const position = element.getBoundingClientRect();
+    const x1 = element.target;
+    console.log(x1);
     
+});
+
+
+
+// let gameArray = [[null, ]];
