2f0934a90bce826613047ae92b62bbe2ea43e446
George Perry <george.e.perry5@gmail.com>
2022-08-08 11:57:26 -0700

    full board complete with alternating square colors

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,39 @@
-let boarder = document.querySelector('.boarder');
-let colorCount = 1;
+///////////////////////////////////////////////////////////////////
+//Board set-up 
+
+let boarder = document.querySelector('.boarder'); //graph boarder of board
+let colorCount = 1; //counter for the color of the div
 
 
-for (i = 1; i <= 64; i++){
+for (i = 1; i <= 64; i++){ //add 64 divs of alternating color
     let div = document.createElement("div");
-    // button.innerHTML = `Do Something ${i}`;
     div.setAttribute("class", `div ${i}`);
+
+    if (colorCount % 9 === 0){
+        console.log("mod 8 " + colorCount);
+        colorCount++;
+         
+    }
 
     if (colorCount % 2 === 1){
         div.style.background = '#FFEBCD';
+        console.log("mod 2 " + colorCount);
         colorCount++;
-        // console.log(colorCount);
+    
     }
     else{
         div.style.background = '#CD853F';
+        console.log("nothing " + colorCount);
         colorCount++;
+ 
     }
+
+   
 
     boarder.appendChild(div);
 }
+
+///////////////////////////////////////////////////////////////////
+
+     
+    
