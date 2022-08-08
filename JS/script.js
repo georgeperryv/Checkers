90c7c79106a2672bc3486589dc249b3e81131935
George Perry <george.e.perry5@gmail.com>
2022-08-08 10:52:11 -0700

    updated display board - need to make the colors alternate and keep the squares the same size regardless of window size

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,21 @@
-// var row1 = document.querySelector('.row-1');
+let boarder = document.querySelector('.boarder');
+let colorCount = 1;
 
-// let count = 1;
-// for (i = 0; i < 8; i++){
-//     var button = document.createElement("button");
-//     button.innerHTML = `Do Something ${i}`;
-//     button.setAttribute("class", `button ${i}`);
 
-//     if (count % 2 === 1){
-//         button.style.background = 'white';
-//         count++;
-//         console.log(count);
-//     }
-//     else{
-//         button.style.background = 'black';
-//         count++;
-//     }
+for (i = 1; i <= 64; i++){
+    let div = document.createElement("div");
+    // button.innerHTML = `Do Something ${i}`;
+    div.setAttribute("class", `div ${i}`);
 
-//     row1.appendChild(button);
-// }
+    if (colorCount % 2 === 1){
+        div.style.background = '#FFEBCD';
+        colorCount++;
+        // console.log(colorCount);
+    }
+    else{
+        div.style.background = '#CD853F';
+        colorCount++;
+    }
+
+    boarder.appendChild(div);
+}
